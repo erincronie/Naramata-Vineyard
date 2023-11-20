dae0234ab7dd5566784c12bfd8e0f29ae80777f5
erincronie <erincronie@gmail.com>
2023-11-20 09:10:21 -0800

    compeleted footer, working on slideshow

---
+++ naramata-vineyard/src/components/Slideshow/Slideshow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Slideshow/Slideshow.scss';
+import { slide1 } from '../../../assets/images/pool-and-lake.png'
+
+const Slideshow = () => {
+    return (
+        <section>
+            <img src={slide1} alt="" />
+        </section>
+    )
+}
