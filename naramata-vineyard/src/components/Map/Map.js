e83f68e43a96ebe6dca0fb506f34b1260952242a
erincronie <erincronie@gmail.com>
2023-11-20 14:22:15 -0800

    map intergrated

--- naramata-vineyard/src/components/Map/Map.js
+++ naramata-vineyard/src/components/Map/Map.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from 'react';
+import '../../components/Map/Map.scss';
 
 const Map = () => {
+
   return (
-    <div>
+    <div className='map'>
+        <h2 className='map__title'>Location</h2>
+        <p classname='map__lanes'>plus bike lanes, trails and footways</p>
+      <iframe className='map__details' width="100%" height="450" frameborder="0" title="Felt Map" src="https://felt.com/embed/map/Naramata-7XDdchwrTpuB5tJDLesGhB?loc=49.58481,-119.58233,14.84z"></iframe>
     </div>
   );
 };
--- naramata-vineyard/src/pages/Home/Home.js
+++ naramata-vineyard/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import '../Home/Home.scss'
 import React from "react";
 import Slideshow from '../../components/Slideshow/Slideshow';
 import Map from '../../components/Map/Map';
+import Pool from '../../assets/images/gradient-pool.jpeg'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Slideshow />
             <h2 className='homepage__title'>Namasté Naramata</h2>
             <p className='homepage__bio'>Indulge in the tranquility of our bed and breakfast nestled within a vineyard's embrace, where the subtle whispers of rustling vines and the serene ambiance of the rolling countryside create a peaceful escape. Our quiet retreat on the vineyard offers a calming haven for guests, inviting them to unwind amid the picturesque beauty of the winemaking landscape</p>
+            <img className='homepage__image'src={Pool} alt="Pool and deck" />
             <Map />
         </main>
     );
