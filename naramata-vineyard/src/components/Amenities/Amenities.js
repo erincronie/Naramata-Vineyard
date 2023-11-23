e0fd6581e029d80f494ba96952d17879b1b76835
erincronie <erincronie@gmail.com>
2023-11-23 14:21:41 -0800

    created amenitie json file

--- naramata-vineyard/src/components/Amenities/Amenities.js
+++ naramata-vineyard/src/components/Amenities/Amenities.js
@@ -1,4 +1,3 @@
-// Amenities.jsx
 import React from 'react';
 import amenitiesData from '../../data/amenities.json';
 import '../Amenities/Amenities.scss';
@@ -11,7 +10,7 @@ const Amenities = () => {
           const iconString = amenitiesData[section].icon;
           return (
             <div className='amenities__section' key={index}>
-              <div dangerouslySetInnerHTML={{ __html: iconString }} className='amenities__icons' />
+              <div className='amenities__icons' dangerouslySetInnerHTML={{ __html: iconString }} />
               <h3 className='amenities__titles'>{amenitiesData[section].title}:</h3>
               {amenitiesData[section].features.map((feature, featureIndex) => (
                 <p className='amenities__features' key={featureIndex}>{feature}</p>
